f48d251495844ea19c357e58ca191abc127e31be
DESKTOP-ROVQ1IK\ArhanShahid <arhanshahid@gmail.com>
2017-08-02 18:37:33 +0500

    update

--- scripts/appController.js
+++ scripts/appController.js
@@ -1,55 +1,42 @@
 app.controller('appController', ['$scope', 'fbService', function ($scope, fbService) {
 
-    $scope.dataList = [];
-
-    var db = fbService.db;
     var data = fbService.data;
-
-    data.once('value').then(function (res) {
-        res.forEach(function (item) {
-            $scope.dataList.push({
-                id: item.key,
-                name: item.val().name,
-                contact: item.val().contact
-            })
-        });
-        $scope.$apply();
-    });
-
-    data.on('child_added', function (res) {
-        // $scope.dataList.push({
-        //     id: res.key,
-        //     name: res.val().name,
-        //     contact: res.val().contact
-        // });
-    });
-
-    data.on('child_removed', function (res) {
-        console.log('child_removed')
-    })
-
+    $scope.dataList = [];
     $scope.showEdit = false;
-
-
-    $scope.editname = {
-        $id: null,
-        first: null,
-        last: null
+    $scope.editData = {
+        id: null,
+        name: null,
+        contact: null
+    };
+    $scope.newData = {
+        name: null,
+        contact: null
     };
 
-    $scope.name = {
-        first: null,
-        last: null
-    };
+    function getData() {
+        $scope.dataList = [];
+        data.once('value').then(function (res) {
+            res.forEach(function (item) {
+                $scope.dataList.push({
+                    id: item.key,
+                    name: item.val().name,
+                    contact: item.val().contact
+                })
+            });
+            $scope.$apply();
+        });
+    }
+    getData();
 
     $scope.save = function () {
         data.push({name: 'Arhan', contact: '0333333333'});
+        getData();
     };
-    $scope.enableEdit = function (name) {
+    $scope.enableEdit = function (data) {
         $scope.showEdit = true;
-        $scope.editname.$id = name.$id;
-        $scope.editname.first = name.first;
-        $scope.editname.last = name.last;
+        $scope.editData.$id = data.id;
+        $scope.editData.name = data.name;
+        $scope.editData.contact = data.contact;
 
     };
 
@@ -62,14 +49,14 @@ app.controller('appController', ['$scope', 'fbService', function ($scope, fbServ
 
     $scope.delete = function (obj) {
         data.child(obj.id).remove().then(function () {
-            $scope.dataList.splice(search(obj.id, $scope.dataList), 1)
+            $scope.dataList.splice(search(obj.id, $scope.dataList), 1);
             $scope.$apply();
         });
     };
 
     function search(nameKey, myArray) {
         for (var i = 0; i < myArray.length; i++) {
-            if (myArray[i].id === nameKey) {
+            if (myArray[i].id == nameKey) {
                 return i;
             }
         }
