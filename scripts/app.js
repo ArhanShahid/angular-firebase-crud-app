456d7d7d81c5418e2f00f66d96320f30449b5310
Arhan Shahid <arhanshahid@gmail.com>
2017-08-12 10:21:04 +0500

    app changed

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,12 @@
 var app = angular.module('app', ['firebase']);
 
 app.constant('FIREBASE_CONFIG', {
-    apiKey: "AIzaSyB2T6Q7jrx-lx0bxHmhvMxFSIHHW9ldgsg",
-    authDomain: "fir-crud-sample.firebaseapp.com",
-    databaseURL: "https://fir-crud-sample.firebaseio.com",
-    projectId: "fir-crud-sample",
-    storageBucket: "fir-crud-sample.appspot.com",
-    messagingSenderId: "183757689941"
+    apiKey: "AIzaSyDOUd98mu9fwR9-UEKKtI4KleQus5LVET0",
+    authDomain: "fir-crud-sample-d2696.firebaseapp.com",
+    databaseURL: "https://fir-crud-sample-d2696.firebaseio.com",
+    projectId: "fir-crud-sample-d2696",
+    storageBucket: "",
+    messagingSenderId: "748482557474"
 });
 app.config(function () {
 });
